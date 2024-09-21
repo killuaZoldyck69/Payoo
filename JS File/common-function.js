fdd533a21d1c0574f03ecfd26729122ce1276b91
killuaZolyck69 <tanver694225@gmail.com>
2024-09-21 22:11:54 +0600

    completeadd money and cash out function

---
+++ JS File/common-function.js
@@ -0,0 +1,13 @@
+// get element by id
+function getEleID(id) {
+  return document.getElementById(id);
+}
+
+// get input form value
+function getValue(id) {
+  return document.getElementById(id).value;
+}
+
+function getInnerText(id) {
+  return document.getElementById(id).innerText;
+}
---
+++ JS File/utility.js
@@ -0,0 +1,36 @@
+// cash out button
+getEleID("cash-out-btn").addEventListener("click", function (event) {
+  event.preventDefault();
+
+  getEleID("amount-title").innerText = "Cash Out";
+  getEleID("transactional-btn").innerText = "Cash Out";
+});
+
+// Add money button
+getEleID("add-money-btn").addEventListener("click", function (event) {
+  event.preventDefault();
+
+  getEleID("amount-title").innerText = "Add Money";
+  getEleID("transactional-btn").innerText = "Add Money";
+});
+
+getEleID("transactional-btn").addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const amount = parseFloat(getValue("amount"));
+  const pinNum = getValue("pin");
+  const currentBalance = parseFloat(getInnerText("current-balance"));
+
+  if (pinNum === "1234" && getInnerText("transactional-btn") === "Add Money") {
+    const newBalance = amount + currentBalance;
+    getEleID("current-balance").innerText = newBalance;
+  } else if (
+    pinNum === "1234" &&
+    getInnerText("transactional-btn") === "Cash Out"
+  ) {
+    const newBalance = currentBalance - amount;
+    getEleID("current-balance").innerText = newBalance;
+  } else {
+    alert("Transaction Failed");
+  }
+});
